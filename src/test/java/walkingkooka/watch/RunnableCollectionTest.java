933e9a88c7e758bdff26a30ab4fdacda2ef591f1
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-03-17 20:24:25 +1100

    RunnableCollection

---
+++ src/test/java/walkingkooka/watch/RunnableCollectionTest.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.watch;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.ToStringTesting;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.reflect.ClassTesting2;
+import walkingkooka.reflect.JavaVisibility;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class RunnableCollectionTest implements ToStringTesting<RunnableCollection>,
+    ClassTesting2<RunnableCollection> {
+
+    @Test
+    public void testWithNullFails() {
+        assertThrows(
+            NullPointerException.class,
+            () -> RunnableCollection.with(null)
+        );
+    }
+
+    @Test
+    public void testWithEmptyFails() {
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> RunnableCollection.with(
+                Lists.empty()
+            )
+        );
+    }
+
+    @Test
+    public void testWithOneNeverWraps() {
+        final Runnable runnable = () -> {};
+
+        assertSame(
+            runnable,
+            RunnableCollection.with(
+                Lists.of(runnable)
+            )
+        );
+    }
+
+    @Test
+    public void testWith() {
+        final Runnable runnable1 = () -> {};
+        final Runnable runnable2 = () -> {};
+
+        final RunnableCollection runnableCollection = (RunnableCollection) RunnableCollection.with(
+            Lists.of(
+                runnable1,
+                runnable2
+            )
+        );
+    }
+
+    // run..............................................................................................................
+
+    @Test
+    public void testRun() {
+        final AtomicInteger counter = new AtomicInteger();
+        final Runnable runnable1 = () -> counter.incrementAndGet();
+        final Runnable runnable2 = () -> counter.incrementAndGet();
+
+        final Runnable runnableCollection = RunnableCollection.with(
+            Lists.of(
+                runnable1,
+                runnable2
+            )
+        );
+
+        runnableCollection.run();
+
+        this.checkEquals(
+            2,
+            counter.get()
+        );
+    }
+
+    // toString.........................................................................................................
+
+    @Test
+    public void testToString() {
+        final AtomicInteger counter = new AtomicInteger();
+        final Runnable runnable1 = new Runnable() {
+
+            @Override
+            public void run() {
+
+            }
+
+            @Override
+            public String toString() {
+                return "11";
+            }
+        };
+        final Runnable runnable2 = new Runnable() {
+
+            @Override
+            public void run() {
+
+            }
+
+            @Override
+            public String toString() {
+                return "22";
+            }
+        };
+
+        this.toStringAndCheck(
+            RunnableCollection.with(
+                Lists.of(
+                    runnable1,
+                    runnable2
+                )
+            ),
+            "[11, 22]"
+        );
+    }
+
+    // class............................................................................................................
+
+    @Override
+    public Class<RunnableCollection> type() {
+        return RunnableCollection.class;
+    }
+
+    @Override
+    public JavaVisibility typeVisibility() {
+        return JavaVisibility.PACKAGE_PRIVATE;
+    }
+}
